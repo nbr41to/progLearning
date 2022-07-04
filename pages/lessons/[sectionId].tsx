2a655a72aa92dff102f96473c0a3a440c16c29fe
Knob <59198478+nbr41to@users.noreply.github.com>
2022-07-04 18:29:25 +0900

    Update [sectionId].tsx

--- pages/lessons/[sectionId].tsx
+++ pages/lessons/[sectionId].tsx
@@ -1,6 +1,6 @@
 import type {
-  // GetStaticPaths,
-  // GetStaticProps,
+  GetStaticPaths,
+  GetStaticProps,
   GetStaticPropsContext,
   InferGetStaticPropsType,
   NextPage,
@@ -8,25 +8,25 @@ import type {
 import Head from 'next/head';
 import {
   getCategories,
-  // getSectionContentById,
+  getSectionContentById,
   getSections,
 } from 'notion/lessons';
 import { SWRConfig } from 'swr';
 
-// import { SectionPage } from '@/components/@pages/SectionPage';
+import { SectionPage } from '@/components/@pages/SectionPage';
 
 export const getStaticProps = async (
   context: GetStaticPropsContext<{ sectionId: string }>,
 ) => {
-  // const sectionId = context.params?.sectionId || '';
+  const sectionId = context.params?.sectionId || '';
 
-  // const section = await getSectionContentById(sectionId);
+  const section = await getSectionContentById(sectionId);
   const categories = await getCategories();
   const sections = await getSections();
 
   return {
     props: {
-      // section,
+      section,
       fallback: {
         '/lessons/categories': categories,
         '/lessons/sections': sections,
@@ -35,27 +35,27 @@ export const getStaticProps = async (
   };
 };
 
-// export const getStaticPaths = async () => {
-//   const sections = await getSections();
-//   const paths = sections.map(({ id }) => ({
-//     params: { sectionId: id || '' },
-//   }));
-//   return {
-//     paths,
-//     fallback: 'blocking', // HTMLを生成しない
-//   };
-// };
+export const getStaticPaths = async () => {
+  const sections = await getSections();
+  const paths = sections.map(({ id }) => ({
+    params: { sectionId: id || '' },
+  }));
+  return {
+    paths,
+    fallback: 'blocking', // HTMLを生成しない
+  };
+};
 
 type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-const Section: NextPage<Props> = ({ fallback }) => {
+const Section: NextPage<Props> = ({ section, fallback }) => {
   return (
     <>
       <Head>
         <title>Section | progLearning</title>
       </Head>
       <SWRConfig value={{ fallback }}>
-        {/* <SectionPage section={section} /> */}
+        <SectionPage section={section} />
       </SWRConfig>
     </>
   );
